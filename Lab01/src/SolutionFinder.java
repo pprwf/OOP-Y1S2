2b2c12195b0a5c5ae8262f86f85801567ff8ee6c
pprwf <109953609+pprwf@users.noreply.github.com>
2023-01-20 13:00:39 +0700

    Lab Week01

---
+++ Lab01/src/SolutionFinder.java
@@ -0,0 +1,7 @@
+public class SolutionFinder {
+    public static void main(String[] args) {
+        double a = 4, b = 8, c = 3;
+        x1 = (- b + Math.sqrt((b * b) - 4 * a * c)) / (2 * a);
+        x2 = 
+}
+}
