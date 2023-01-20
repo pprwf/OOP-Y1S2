44d12f2bb67a29cbdcc30529f620ee6447269474
pprwf <109953609+pprwf@users.noreply.github.com>
2023-01-20 14:33:17 +0700

    Delete Lab01 directory

--- Lab01/src/SolutionFinder.java
+++
@@ -1,7 +0,0 @@
-public class SolutionFinder {
-    public static void main(String[] args) {
-        double a = 4, b = 8, c = 3;
-        x1 = (- b + Math.sqrt((b * b) - 4 * a * c)) / (2 * a);
-        x2 = 
-}
-}
