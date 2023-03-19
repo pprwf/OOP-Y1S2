2a194d098df1105750fce2a9ef3cc1270232bf22
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-03-20 02:29:05 +0700

    Bank.java: Incompleted

---
+++ Lab/Lab08/Coding/src/Bank.java
@@ -0,0 +1,16 @@
+public class Bank {
+      private Account acct [];
+      private int numAcct;
+      public Bank () {
+            
+      }
+      public void addAccount (Account ac) {
+            
+      }
+      public Account getAccount (int index) {
+            return this.acct;
+      }
+      public int getNumAcct () {
+            return this.numAcct;
+      }
+}
