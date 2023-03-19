4e8e82d5c64ced6217561a09154c2e6c01946686
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-03-20 02:24:17 +0700

    Lab | Week 08 (Customer.java)

---
+++ Lab/Lab08/Coding/src/Customer.java
@@ -0,0 +1,43 @@
+public class Customer {
+      private String firstName;
+      private String lastName;
+      private CheckingAccount acct;
+      public Customer () {
+            this("", "", null);
+      }
+      public Customer (String firstName, String lastName) {
+            this(firstName, lastName, null);
+      }
+      public Customer (String firstName, String lastName, CheckingAccount acct) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.acct = acct;
+      }
+      public void setFirstName (String firstName) {
+            this.firstName = firstName;
+      }
+      public String getFirstName () {
+            return this.firstName;
+      }
+      public void setLastName (String lastName) {
+            this.lastName = lastName;
+      }
+      public String getLastName () {
+            return this.lastName;
+      }
+      public void setAcct (CheckingAccount acct) {
+            this.acct = acct;
+      }
+      public CheckingAccount getAcct () {
+            return this.acct;
+      }
+      public String toString () {
+            if (acct == null) {
+                  return this.getFirstName() + " " + this.getLastName() + " doesn’t have account.";
+            }
+            return "The " + this.getFirstName() + " account has " + this.getAcct().getBalance() + " baht and " + this.getAcct().getCredit() + " credits.";
+      }
+      public boolean equals (Customer c) {
+            return (this.getFirstName() + this.getLastName()).equals(c.getFirstName() + c.getLastName());
+      }
+}
