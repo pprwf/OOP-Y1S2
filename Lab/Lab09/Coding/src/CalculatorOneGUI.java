e55f714acf26062de5f02e80538dc56dfedb19d5
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-04-05 23:01:37 +0700

    Lab | Week 08

--- Lab/Lab09/Coding/src/CalculatorOneGUI.java
+++ Lab/Lab09/Coding/src/CalculatorOneGUI.java
@@ -1,5 +1,32 @@
 import java.awt.*;
+import javax.swing.*;
 
 public class CalculatorOneGUI {
-      
+      public CalculatorOneGUI () {
+            JFrame fr = new JFrame("เครื่องคิดเลข");
+            JPanel p = new JPanel();
+            JButton b1 = new JButton("บวก");
+            JButton b2 = new JButton("ลบ");
+            JButton b3 = new JButton("คูณ");
+            JButton b4 = new JButton("หาร");
+            JTextField txt1 = new JTextField();
+            JTextField txt2 = new JTextField();
+            JTextField txt3 = new JTextField();
+            
+            fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fr.setLayout(new GridLayout(4, 0));
+            fr.setBounds(600, 350, 300, 150);
+            p.setLayout(new FlowLayout());
+            
+            p.add(b1);
+            p.add(b2);
+            p.add(b3);
+            p.add(b4);
+            fr.add(txt1);
+            fr.add(txt2);
+            fr.add(p);
+            fr.add(txt3);
+            
+            fr.setVisible(true);
+      }
 }
