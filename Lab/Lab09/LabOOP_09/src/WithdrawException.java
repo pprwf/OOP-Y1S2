1bc5f6bdc4f515d5141aeaa48712f986286b5b1a
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-04-24 02:35:31 +0700

    Lab | Week 09 (Exam 1)

--- Lab/Lab09/LabOOP_09/src/WithdrawException.java
+++
@@ -1,8 +0,0 @@
-public class WithdrawException extends Exception {
-    public WithdrawException () {
-        super();
-    }
-    public WithdrawException (String s) {
-        super(s);
-    }
-}
