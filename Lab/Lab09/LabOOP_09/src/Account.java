19f46ad4ae13f295ebb8ab8b0c9fa1f2ab6bdc51
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-04-24 02:37:18 +0700

    Lab | Week 09 (Exam 2)

---
+++ Lab/Lab09/LabOOP_09/src/Account.java
@@ -0,0 +1,44 @@
+public class Account {
+      protected double balance;
+      protected String name;
+      public Account (double balance, String name) {
+            this.balance = balance;
+            this.name = name;
+      }
+      public void deposit (double a) {
+            if (a > 0) {
+                  System.out.println(a + " baht is deposited to " + this.getName() + ".");
+                  this.setBalance(this.getBalance() + a);
+            }
+            else {
+                  System.out.println("Input number must be a positive integer.");
+            }
+      }
+      public void withdraw (double a) throws WithdrawException {
+            if (a < 0) {
+                  System.out.println("Input number must be a positive integer.");
+            }
+            else if (this.getBalance() - a < 0) {
+                  throw new WithdrawException("Account " + this.name + " has not enough money.");
+            }
+            else {
+                  System.out.println(a + " baht is withdrawn from " + this.getName() + ".");
+                  this.setBalance(this.getBalance() - a);
+            }
+      }
+      public void setName (String name) {
+            this.name = name;
+      }
+      public String getName () {
+            return this.name;
+      }
+      public void setBalance (double balance) {
+            this.balance = balance;
+      }
+      public double getBalance () {
+            return this.balance;
+      }
+      public void showAccount () {
+            System.out.println(this.getName() + " account has " + this.getBalance() + " baht.");
+      }
+}
---
+++ Lab/Lab09/LabOOP_09/src/CheckingAccount.java
@@ -0,0 +1,43 @@
+public class CheckingAccount extends Account{
+      private double credit;
+      public CheckingAccount () {
+            super(0, "");
+            this.credit = 0;
+      }
+      public CheckingAccount (double balance, String name, double credit) {
+            super(balance, name);
+            this.credit = credit;
+      }
+      public void setCredit (double credit) {
+            if (credit > 0) {
+                  this.credit = credit;
+            }
+            else {
+                  System.out.println("Input number must be a positive integer.");
+            }
+      }
+      public double getCredit () {
+            return this.credit;
+      }
+      public void withdraw (double amount) throws WithdrawException {
+            if (this.getBalance() + this.getCredit() - amount < 0) {
+                  throw new WithdrawException("Account " + this.name + " has not enough money.");
+            }
+            else {
+                  if (this.getBalance() - amount >= 0) {
+                        this.setBalance(this.getBalance() - amount);
+                  }
+                  else {
+                        this.setCredit(this.getCredit() - (amount - this.getBalance()));
+                        this.setBalance(0);
+                  }
+                  System.out.println(amount + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
+            }
+      }
+      public void withdraw (String amount) throws WithdrawException {
+            withdraw (Double.parseDouble(amount));
+      }
+      public String toString () {
+            return "The " + this.getName() + " account has " + this.getBalance() + " baht and " + this.getCredit() + " credits.";
+      }
+}
