607f750d2f008dab62a4670a871e0c749c232c9c
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-04-24 02:49:15 +0700

    Lab | Week 10 (Exam 1)

---
+++ Lab/Lab10/LabOOP_10/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new CalculatorOneGUI();
+        
+//        new CalculatorTwoGUI();
+        
+//        new TellerGUI();
+        
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } 
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        SwingUtilities.invokeLater(() -> {MDIFromGUI ui = new MDIFromGUI();});
+    }
+}
